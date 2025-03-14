e4b09a5c68c25dfc98f4049633f7a295923e7b1b
greenputola <tribetoy2025@gmail.com>
2025-03-14 14:31:14 +0530

    update

--- utils/api.js
+++ utils/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 
-const API_BASE_URL = process.env.BASE_URL || "http://localhost:4000";
+const API_BASE_URL = process.env.BASE_URL;
 
 export const fecthDataFormApi=async(url)=>{
   try {
@@ -44,40 +44,4 @@ export const deleteData = async (url) => {
   }
 };
 
-
-
-
-
-
-
-
-// import axios from "axios";
-// // require('dotenv/config');
-
-// export const fecthDataFormApi=async(url) =>{
-//     try{
-//         const {data} = await axios.get("http://localhost:4000"+url)
-//         return data;
-//     }
-//     catch(error){
-//         console.log(error);
-//         return error;
-//     }
-// }
-
-// export const postData = async (url, formData) => {
-//         const { res } = await axios.post("http://localhost:4000" + url, formData);
-//         return res;
-    
-// };
-
-// export const editData = async (url, updateData) =>{
-//     const { res } = await axios.put(`http://localhost:4000${url}`, updateData)
-//     return res;
-// }
-
-// export const deleteData = async (url) =>{
-//     const { res } =await axios.delete(`http://localhost:4000${url}`)
-//     return res;
-// }
 
